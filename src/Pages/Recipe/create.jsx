931eef786d92265be02a108a22865cde145a9b2f
Roosevelt Blow <rooseveltblow@gmail.com>
2023-01-30 11:31:43 -0700

    renders create recipe form

--- src/Pages/Recipe/create.jsx
+++ src/Pages/Recipe/create.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import FormButton from '../../Components/Forms/Button';
 
 const CreateRecipe = ()=>{
     return(
-        <h1>Create Recipe</h1>
+        <div>
+            <h2>Create New Recipe</h2>
+            <form className='recipe-form'>
+                <label htmlFor="title">Title:</label>
+                <input type="text" name="title" id="title" />
+                <label htmlFor="ingredients">Ingredients:</label>
+                <textarea name="ingredients" id="ingredients" cols="30" rows="10"></textarea>
+                <label htmlFor="directions">Directions:</label>
+                <textarea name="directions" id="directions" cols="30" rows="10"></textarea>
+                <FormButton type="submit" value="Create Recipe" />
+            </form>
+        </div>
     )
 }
 
